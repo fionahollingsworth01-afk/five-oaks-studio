420c337de8cff3f6d66038fc608300a90293eef1
fionahollingsworth01-afk <fionahollingsworth01@gmail.com>
2026-07-25 16:30:56 -0400

    Add offline app support

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='five-oaks-studio-v1';
+const FILES=['./','index.html','manifest.json'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(saved=>saved||fetch(event.request)));});
